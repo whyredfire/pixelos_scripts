PixelOS-AOSP/android_vendor_custom

repopick: try fetching pixelos gerrit changes from GitHub first
Change-Id: Ibd19160075cfa2b768365abbe5158a683a769e79
Signed-off-by: inferno0230 <mail@inferno0230.in>

build/tools/repopick.py
@@ -198,6 +198,10 @@ def is_lineage_gerrit(remote_url):
     p = urllib.parse.urlparse(remote_url)
     return p.hostname == "review.lineageos.org"
 
+def is_pixelos_gerrit(remote_url):
+    p = urllib.parse.urlparse(remote_url)
+    return p.hostname == "review.pixelos.org"
+
 
 def commit_exists(project_path, revision):
     return (
@@ -657,7 +661,7 @@ def do_git_fetch_pull(args, item):
     cmd.extend(["", item["fetch"][method]["ref"]])
 
     # Try fetching from GitHub first if using lineage gerrit
-    if is_lineage_gerrit(args.gerrit):
+    if is_lineage_gerrit(args.gerrit) or is_pixelos_gerrit(args.gerrit):
         if args.verbose:
             print("Trying to fetch the change from GitHub")
 

whyredfire/pixelos_scripts

repopick/repopick.py
@@ -187,6 +187,10 @@ def is_lineage_gerrit(remote_url):
     p = urllib.parse.urlparse(remote_url)
     return p.hostname == "review.lineageos.org"
 
+def is_pixelos_gerrit(remote_url):
+    p = urllib.parse.urlparse(remote_url)
+    return p.hostname == "review.pixelos.net"
+
 
 def commit_exists(project_path, revision):
     return (
@@ -646,7 +650,7 @@ def do_git_fetch_pull(args, item):
     cmd.extend(["", item["fetch"][method]["ref"]])
 
     # Try fetching from GitHub first if using lineage gerrit
-    if is_lineage_gerrit(args.gerrit):
+    if is_lineage_gerrit(args.gerrit) or is_pixelos_gerrit(args.gerrit):
         if args.verbose:
             print("Trying to fetch the change from GitHub")
 